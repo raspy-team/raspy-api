b235544b401842e1111a9c1efb349905feb69736
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-04 20:31:25 +0900

    modify directory name

--- src/main/kotlin/com/raspy/backend/DemoApplication.kt
+++ src/main/kotlin/com/raspy/backend/DemoApplication.kt
@@ -1,4 +1,4 @@
-package com.cc.demo
+package com.raspy.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/raspy/backend/DemoApplicationTests.kt
+++ src/test/kotlin/com/raspy/backend/DemoApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.cc.demo
+package com.raspy.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
