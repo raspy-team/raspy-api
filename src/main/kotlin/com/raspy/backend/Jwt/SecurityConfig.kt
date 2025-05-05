81f45f4a0084459057d62079d5ba1a1c27070118
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-05 17:32:42 +0900

    create configuration file for security

---
+++ src/main/kotlin/com/raspy/backend/Jwt/SecurityConfig.kt
@@ -0,0 +1,35 @@
+package com.raspy.backend.Jwt
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers("/h2-console/**").permitAll() // H2 콘솔
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .anyRequest().hasAnyRole("USER", "ADMIN")
+            }
+            .headers { it.frameOptions { it.sameOrigin() } } // H2 콘솔이 Iframe이라 허용해줘야 브라우저에서 작동함.
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
