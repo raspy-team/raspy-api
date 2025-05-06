6770f2f6535b308970441e04edd90286369c8be6
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-06 16:02:17 +0900

    feat : add validation conditions

--- src/main/kotlin/com/raspy/backend/auth/Request/LoginRequest.kt
+++ src/main/kotlin/com/raspy/backend/auth/Request/LoginRequest.kt
@@ -1,8 +1,16 @@
 package com.raspy.backend.auth.Request
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class LoginRequest(
-    @field:NotBlank val email: String,
-    @field:NotBlank val password: String
+    @field:NotBlank
+    @field:Email
+    @field:Size(max = 50)
+    val email: String,
+
+    @field:NotBlank
+    @field:Size(min = 8, max = 20)
+    val password: String
 )
