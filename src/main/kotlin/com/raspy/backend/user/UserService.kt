563c9e2964786122eae082bdf6427db8a69f954a
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-06 15:43:09 +0900

    feat : create user service

---
+++ src/main/kotlin/com/raspy/backend/user/UserService.kt
@@ -0,0 +1,13 @@
+package com.raspy.backend.user
+
+import com.raspy.backend.user_profile.enumerated.Gender
+import com.raspy.backend.user_profile.enumerated.Region
+import com.raspy.backend.user_profile.UserProfileEntity
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    // private val userRepository: UserRepository
+) {
+
+}
