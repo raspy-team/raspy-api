d88803559d0dfe716aaae05f7cd92fa6869ad5c3
woooo <99981511+xhae000@users.noreply.github.com>
2025-05-10 15:10:41 +0900

    refactor : remove unused importation

--- src/main/kotlin/com/raspy/backend/web_socket/WebSocketConfig.kt
+++ src/main/kotlin/com/raspy/backend/web_socket/WebSocketConfig.kt
@@ -10,19 +10,13 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.messaging.simp.stomp.StompCommand
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.messaging.support.ChannelInterceptor
-import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.socket.WebSocketSession
-import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
-import org.springframework.web.socket.server.HandshakeInterceptor
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
 
-import java.security.Principal
 import java.util.concurrent.ConcurrentHashMap
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration
 import org.springframework.web.socket.handler.WebSocketHandlerDecorator
